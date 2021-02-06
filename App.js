325949ccab219a99603567d2e9342874a9e20308
shanika1999 <shanika.18@itfac.mrt.ac.lk>
2021-02-07 03:13:17 +0530

    qr code screenn

---
+++ App.js
@@ -0,0 +1,275 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+/*
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+/*
+
+
+<TextInput
+          style={styles.textInputStyle}
+          onChangeText={
+            (inputText) => setInputText(inputText)
+          }
+          placeholder="Enter Any Value"
+          value={inputText}
+        />
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => setQrvalue(inputText)}>
+          <Text style={styles.buttonTextStyle}>
+            Generate QR Code
+          </Text>
+        </TouchableOpacity>
+
+
+
+
+
+
+
+
+
+        */
+
+
+// Generation of QR Code in React Native
+// https://aboutreact.com/generation-of-qr-code-in-react-native/
+
+// import React in our code
+//import React, {useState} from 'react';
+import React, { Component } from 'react'
+
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  
+} from 'react-native';
+
+import QRCode from 'react-native-qrcode-svg';
+
+export default class App extends Component {   
+  //const [inputText, setInputText] = useState('');
+  //const [qrvalue, setQrvalue] = useState('');
+
+  //const [state, setState] = useState();
+  state = {
+    text: '5555',
+  };
+
+  render(){
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={styles.titleStyle}>
+          see your QR Code here
+        </Text>
+        <QRCode
+          //QR code value
+          
+          //value={qrvalue ? qrvalue : 'NA'}
+          onChangeText={(text) => this.setState({text: text})}
+          value={this.state.text}
+          //size of QR Code
+          size={250}
+          //Color of the QR Code (Optional)
+          color="black"
+          //Background Color of the QR Code (Optional)
+          backgroundColor="white"
+          //Logo of in the center of QR Code (Optional)
+          logo={{
+            url:
+              'https://raw.githubusercontent.com/AboutReact/sampleresource/master/logosmalltransparen.png',
+          }}
+          //Center Logo size  (Optional)
+          logoSize={30}
+          //Center Logo margin (Optional)
+          logoMargin={2}
+          //Center Logo radius (Optional)
+          logoBorderRadius={15}
+          //Center Logo background (Optional)
+          logoBackgroundColor="yellow"
+        />
+        
+
+
+
+
+        
+      </View>
+    </SafeAreaView>
+  );
+};
+}
+//export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,
+  },
+  titleStyle: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  textStyle: {
+    textAlign: 'center',
+    margin: 10,
+  },
+  textInputStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#51D8C7',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#51D8C7',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 30,
+    padding: 10,
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+});
